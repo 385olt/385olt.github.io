0e846d3c4f0d4a641ef7a72db79f2bd2f5faddc4
evb <385olt@gmail.com>
2018-03-03 23:49:21 +0200

    gameover update

--- mine/gameover.js
+++ mine/gameover.js
@@ -12,11 +12,11 @@ Donate.GameOver.prototype = {
 		
 		this.add.text(this.world.width/2 - 130, this.world.height/2 - 64, 'Your score is', { font: '32pt Arial', fill: '#000' });
 		
-		var scoreString = this.score.toString();
+		var scoreString = precisionRound(this.score, 2).toString();
 		this.add.text(this.world.width/2 - 20 * scoreString.length, this.world.height/2 - 10, scoreString, { font: '64pt Arial', fill: '#000' });
 		
 		var restartString = "Click to restart!";
-		this.add.text(this.world.width/2 - 6 * scoreString.length, this.world.height - 100, restartString, { font: '16pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 6 * scoreString.length, this.world.height - 100, restartString, { font: '20pt Arial', fill: '#000' });
 	},
 	
 	update: function() {
