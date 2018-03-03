4333969283066e7bd7ce75d4951b06e9a73df74f
evb <385olt@gmail.com>
2018-03-03 23:34:31 +0200

    gameover update

--- mine/gameover.js
+++ mine/gameover.js
@@ -10,8 +10,8 @@ Donate.GameOver.prototype = {
 	
 		this.add.sprite(0, 0, 'sky');
 		
-		this.add.text(this.world.width/2 - 100, this.world.height/2 - 64, 'Your score is', { fontSize: '32px', fill: '#000' });
-		this.add.text(this.world.width/2 - 50, this.world.height/2, this.score.toString(), { fontSize: '64px', fill: '#000' });
+		this.add.text(this.world.width/2 - 100, this.world.height/2 - 64, 'Your score is', { font: '32pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 50, this.world.height/2, this.score.toString(), { font: '64pt Arial', fill: '#000' });
 		
 	},
 	
