c87981a154049ef6af1af83a8e59bcdeb5397123
evb <385olt@gmail.com>
2018-03-03 23:52:03 +0200

    gameover update

--- mine/gameover.js
+++ mine/gameover.js
@@ -16,7 +16,7 @@ Donate.GameOver.prototype = {
 		this.add.text(this.world.width/2 - 20 * scoreString.length, this.world.height/2 - 10, scoreString, { font: '64pt Arial', fill: '#000' });
 		
 		var restartString = "Click to restart!";
-		this.add.text(this.world.width/2 - 6 * scoreString.length, this.world.height - 100, restartString, { font: '20pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 6 * restartString.length, this.world.height - 100, restartString, { font: '20pt Arial', fill: '#000' });
 	},
 	
 	update: function() {
