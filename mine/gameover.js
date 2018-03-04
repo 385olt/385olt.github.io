8f7d6797f2274f73b454f26be9dcdb49f21eabaf
evb <385olt@gmail.com>
2018-03-05 00:47:52 +0200

    enemy ai

--- mine/gameover.js
+++ mine/gameover.js
@@ -10,17 +10,17 @@ Donate.GameOver.prototype = {
 	
 		this.add.sprite(0, 0, 'sky');
 		
-		var scoredString = 'Собрано донатов';
-		this.add.text(this.world.width/2 - 7 * scoredString.length, this.world.height/2 - 80, scoredString, { font: '32pt Arial', fill: '#000' });
+		var scoredString = 'Собрано';
+		this.add.text(this.world.width/2 - 8 * scoredString.length, this.world.height/2 - 80, scoredString, { font: '32pt Arial', fill: '#000' });
 		
 		var scoreString = precisionRound(this.score, 2).toString();
-		this.add.text(this.world.width/2 - 20 * scoreString.length, this.world.height/2 - 30, scoreString, { font: '64pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 20 * scoreString.length, this.world.height/2 - 40, scoreString, { font: '64pt Arial', fill: '#000' });
 		
 		var rubString = 'рублей';
-		this.add.text(this.world.width/2 - 7 * rubString.length, this.world.height/2, rubString, { font: '32pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 8 * rubString.length, this.world.height/2 + 20, rubString, { font: '32pt Arial', fill: '#000' });
 		
 		var restartString = "Кликните или нажмите пробел чтобы начать заново";
-		this.add.text(this.world.width/2 - 5 * restartString.length, this.world.height - 100, restartString, { font: '20pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 6 * restartString.length, this.world.height - 100, restartString, { font: '20pt Arial', fill: '#000' });
 	},
 	
 	update: function() {
