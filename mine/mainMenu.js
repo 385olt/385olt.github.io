2be7df3b897d10a1f971714bcf7ef5da84894957
evb <385olt@gmail.com>
2018-03-05 22:36:42 +0200

    add title screen

--- mine/mainMenu.js
+++ mine/mainMenu.js
@@ -24,10 +24,10 @@ Donate.MainMenu.prototype = {
 		// ------- texts
 		
 		var titleString = 'ХРАБРОВ';
-		this.add.text(this.world.width/2 - 36 * titleString.length, this.world.height/2 - 200, titleString, { font: '90pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 37 * titleString.length, this.world.height/2 - 200, titleString, { font: '90pt Arial', fill: '#000' });
 		
 		var subtitleString = 'в поисках жены';
-		this.add.text(this.world.width/2 - 18 * subtitleString.length, this.world.height/2 - 120, subtitleString, { font: '56pt Arial', fill: '#000' });
+		this.add.text(this.world.width/2 - 18 * subtitleString.length, this.world.height/2 - 120, subtitleString, { font: '57pt Arial', fill: '#000' });
 	
 		var startString = "Кликните или нажмите пробел чтобы начать";
 		this.add.text(this.world.width/2 - 7 * startString.length, this.world.height - 100, startString, { font: '20pt Arial', fill: '#000' });
