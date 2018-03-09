10ebc794779ef0060cef59dab7ae49f36a4d3d46
evb <385olt@gmail.com>
2018-03-09 04:35:46 +0200

    lvlbuilder

--- mine/game.js
+++ mine/game.js
@@ -29,7 +29,7 @@ Hrabrov.Game.prototype = {
 		this.stars.enableBody = true;
 		
 		for (var i = 0; i < 3; i++) {
-			this.makeStar();
+			levelBuilder.makeStar();
 		}
 		
 		// ------- enemies
@@ -38,7 +38,7 @@ Hrabrov.Game.prototype = {
 		this.enemies.enableBody = true;
 		
 		for (var i = 0; i < 3; i++) {
-			this.makeEnemy();
+			levelBuilder.makeEnemy();
 		}
 		// ------- score
 		
@@ -175,7 +175,7 @@ Hrabrov.Game.prototype = {
 			var starsNumber = Math.min(5, enemy.starsKilled);
 			for (var i = 0; i < starsNumber; i++) {
 				var rndDirection = Phaser.ArrayUtils.getRandomItem([-1, 1]) * (enemy.width/2);
-				var star = this.makeStar(enemy.x + enemy.width/2 + rndDirection, enemy.y + enemy.height/2)
+				var star = levelBuilder.makeStar(enemy.x + enemy.width/2 + rndDirection, enemy.y + enemy.height/2)
 				star.body.velocity.y = -starBounceY * this.rnd.frac();
 				
 				if (rndDirection > 0) {
@@ -214,66 +214,18 @@ Hrabrov.Game.prototype = {
 		this.timeLeft += this.timeDelta;
 		this.timeLeftText.text = 'Осталось: ' + precisionRound(this.timeLeft, 2) + ' секунд';
 		
-		this.makeStar();
+		levelBuilder.makeStar();
 		
 		if (this.score > this.nextEnemy * 500) {
 			this.nextEnemy += 1;
-			this.makeEnemy();
+			levelBuilder.makeEnemy();
 		}
 		
 		if (this.score > this.nextStar * 50 * (1 - this.timeDelta)) {
 			this.timeDelta += -0.01;	
 			this.nextStar += 1;
-			this.makeStar();
+			levelBuilder.makeStar();
 		}
-	},
-	
-	makeStar: function(x = false, y = false) {
-		if (!x) { 
-			if (this.stars.length < 10) {
-				x = this.player.x + (this.rnd.frac() - 0.5) * 400;
-				if (x < 0) x = 0;
-				if (x > this.world.width - 16) x = this.world.width - 16;
-			} else {
-				x = this.rnd.frac()*(this.world.width - 16);
-			}
-		}		
-		
-		if (!y) {
-			if (this.stars.length < 10) {
-				y = this.player.y + (this.rnd.frac() - 0.5) * 300;
-				if (y < 0) y = 0;
-				if (y > this.world.height - 100) y = this.world.height - 100;
-			} else {
-				y = this.rnd.frac()*(this.world.height - 100); 
-			}
-		}
-		
-		var star = this.stars.create(x, y, 'star');
-		star.body.gravity.y = 100;
-		star.body.bounce.y = 0.7 + Math.random() * 0.2;
-		star.body.collideWorldBounds = true;
-		star.body.drag.x = 10;
-		
-		return star;
-	},
-	
-	makeEnemy: function(x = false, y = false) {
-		if (!x) { x = Math.random()*(this.world.width - 16); }		
-		if (!y) { y = Math.random()*(this.world.height - 100); }
-		
-		var enemy = this.enemies.create(x, y, 'fem');
-		enemy.body.gravity.y = 300;
-		enemy.body.collideWorldBounds = true;
-		
-		enemy.myDirection = Phaser.ArrayUtils.getRandomItem(['left', 'right']);
-		enemy.goodDirection = true;
-		enemy.starsKilled = 0;
-		
-		enemy.animations.add('left', [0, 1, 2, 3], 10, true);
-		enemy.animations.add('right', [5, 6, 7, 8], 10, true);
-	
-		return enemy;
 	}
 	
 };
--- mine/levelBuilder.js
+++ mine/levelBuilder.js
@@ -5,6 +5,8 @@ LevelBuilder = function() {
     
     this.playerImage = 'dude';
     this.platformImage = 'ground';
+    this.starImage = 'star';
+    this.enemyImage = 'fem';
 };
 
 LevelBuilder.prototype = {
@@ -58,7 +60,55 @@ LevelBuilder.prototype = {
 		player.animations.add('right', [5, 6, 7, 8], 10, true);
 		
 		this.level.player = player;
-    }
+    },
+	
+	makeStar: function(x = false, y = false) {
+		if (!x) { 
+			if (this.level.stars.length < 10) {
+				x = this.level.player.x + (this.level.rnd.frac() - 0.5) * 400;
+				if (x < 0) x = 0;
+				if (x > this.level.world.width - 16) x = this.level.world.width - 16;
+			} else {
+				x = this.level.rnd.frac()*(this.level.world.width - 16);
+			}
+		}		
+		
+		if (!y) {
+			if (this.level.stars.length < 10) {
+				y = this.level.player.y + (this.level.rnd.frac() - 0.5) * 300;
+				if (y < 0) y = 0;
+				if (y > this.level.world.height - 100) y = this.level.world.height - 100;
+			} else {
+				y = this.level.rnd.frac()*(this.level.world.height - 100); 
+			}
+		}
+		
+		var star = this.level.stars.create(x, y, this.starImage);
+		star.body.gravity.y = 100;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+		star.body.collideWorldBounds = true;
+		star.body.drag.x = 10;
+		
+		return star;
+	},
+	
+	makeEnemy: function(x = false, y = false) {
+		if (!x) { x = Math.random()*(this.level.world.width - 16); }		
+		if (!y) { y = Math.random()*(this.level.world.height - 100); }
+		
+		var enemy = this.level.enemies.create(x, y, this.enemyImage);
+		enemy.body.gravity.y = 300;
+		enemy.body.collideWorldBounds = true;
+		
+		enemy.myDirection = Phaser.ArrayUtils.getRandomItem(['left', 'right']);
+		enemy.goodDirection = true;
+		enemy.starsKilled = 0;
+		
+		enemy.animations.add('left', [0, 1, 2, 3], 10, true);
+		enemy.animations.add('right', [5, 6, 7, 8], 10, true);
+	
+		return enemy;
+	}
     
 };
 
