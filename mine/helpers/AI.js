7bcc3bc07bbf7a84e8ffc8d0ebfbe070681ccfb7
evb <385olt@gmail.com>
2018-03-11 00:12:29 +0200

    AI

--- mine/helpers/AI.js
+++ mine/helpers/AI.js
@@ -15,7 +15,7 @@ AI.prototype = {
         this.level.enemies.forEach(function(item) {
             item.goodDirection = false;
             
-            if (this.difficulty == 1 && this.level.rnd.frac() < this.walkRandomness * 10) {
+            if (this.difficulty == 1 && this.level.rnd.frac() < this.walkRandomness * 100) {
                 item.goodDirection = true; 
             }
             
