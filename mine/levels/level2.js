0adffa879a41b37d04cb276a9c56dbe4ce8e140e
evb <385olt@gmail.com>
2018-03-11 20:43:04 +0200

    add bullet damage

--- mine/levels/level2.js
+++ mine/levels/level2.js
@@ -94,7 +94,7 @@ Hrabrov.Level2.prototype = {
             enemy.health -= this.bulletDamage;
         }
         
-        console.log(this.enemyMaxHealth);
+        console.log(this.enemyMaxHealth - this.bulletDamage);
         
         if (enemy.health <= 0) {
             enemy.kill();
