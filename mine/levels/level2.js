9d151920f79b34a1f90f8228ad9ada5e152957c6
evb <385olt@gmail.com>
2018-03-15 22:27:12 +0200

    shooting

--- mine/levels/level2.js
+++ mine/levels/level2.js
@@ -3,7 +3,7 @@ Hrabrov.Level2 = function() {
     this.donationGoal = 5000;
     this.maxAchievedTime = 60 * Phaser.Timer.SECOND;
     this.lastShot = 0;
-    this.shootInterval = 0.1;
+    this.shootInterval = 1000;
 };
 
 Hrabrov.Level2.prototype = {
